6d9a90cd0f8b572329593fc3d354828d4232c737
Thi Nguyen <tthinguyen@berkeley.edu>
2022-04-25 22:54:40 +0000

    finished adding function

--- ligotools/utils.py
+++ ligotools/utils.py
@@ -1,5 +1,6 @@
-from numpy import *
+import numpy as np
 from scipy.io import wavfile
+import matplotlib.pyplot as plt
 
 # function to whiten data
 def whiten(strain, interp_psd, dt):
@@ -36,18 +37,9 @@ def reqshift(data,fshift=100,sample_rate=4096):
     z = np.fft.irfft(y)
     return z
 
-def make_plots():
-# plotting changes for the detectors:
-    if det is 'L1': 
-        pcolor='g'
-        strain_whitenbp = strain_L1_whitenbp
-        template_L1 = template_match.copy()
-    else:
-        pcolor='r'
-        strain_whitenbp = strain_H1_whitenbp
-        template_H1 = template_match.copy()
+def plot(time, timemax, tevent, SNR, strain_whitenbp, template_match, template_fft, datafreq, freqs, fs, data_psd, d_eff, pcolor, det, eventname, plottype):
 
-        # -- Plot the result
+
     plt.figure(figsize=(10,8))
     plt.subplot(2,1,1)
     plt.plot(time-timemax, SNR, pcolor,label=det+' SNR(t)')
@@ -57,7 +49,6 @@ def make_plots():
     plt.xlabel('Time since {0:.4f}'.format(timemax))
     plt.legend(loc='upper left')
     plt.title(det+' matched filter SNR around event')
-
         # zoom in
     plt.subplot(2,1,2)
     plt.plot(time-timemax, SNR, pcolor,label=det+' SNR(t)')
@@ -69,7 +60,7 @@ def make_plots():
     plt.xlabel('Time since {0:.4f}'.format(timemax))
     plt.legend(loc='upper left')
     plt.savefig('figures/' + eventname+"_"+det+"_SNR."+plottype)
-
+    
     plt.figure(figsize=(10,8))
     plt.subplot(2,1,1)
     plt.plot(time-tevent,strain_whitenbp,pcolor,label=det+' whitened h(t)')
@@ -92,9 +83,11 @@ def make_plots():
     plt.legend(loc='upper left')
     plt.title(det+' Residual whitened data after subtracting template around event')
     plt.savefig('figures/' + eventname+"_"+det+"_matchtime."+plottype)
-                 
-        # -- Display PSD and template
-        # must multiply by sqrt(f) to plot template fft on top of ASD:
+    
+    
+    # -- Display PSD and template
+            # must multiply by sqrt(f) to plot template fft on top of ASD:
+
     plt.figure(figsize=(10,6))
     template_f = np.absolute(template_fft)*np.sqrt(np.abs(datafreq)) / d_eff
     plt.loglog(datafreq, template_f, 'k', label='template(f)*sqrt(f)')
